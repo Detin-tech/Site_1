e3034c69bae11afba9bbd8fbc38bfdd17487e36e
Detin_Tech <benjamin.hudson4@gmail.com>
2025-09-04 12:03:52 -0400

    Use Lemon Squeezy buy links

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,5 +1,5 @@
 window.checkoutLinks = {
-  student: 'https://prosperspot.lemonsqueezy.com/checkout/buy/606476',
-  standard: 'https://prosperspot.lemonsqueezy.com/checkout/buy/606477',
-  pro: 'https://prosperspot.lemonsqueezy.com/checkout/buy/606478'
+  student: 'https://prosperspotus.lemonsqueezy.com/buy/aa2befc2-ac07-4da0-a601-34cf1e5bff2e?embed=1&discount=0',
+  standard: 'https://prosperspotus.lemonsqueezy.com/buy/1e344447-7932-4d60-bda9-803cba73f9f7?embed=1&discount=0',
+  pro: 'https://prosperspotus.lemonsqueezy.com/buy/0cc965b1-0ae3-4ef2-86d0-6e99e305888a?embed=1&discount=0'
 };
